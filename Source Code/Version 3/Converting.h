e4a69f5e7b994e5ba5bdc5838a0c88244c8bf1bb
ManavD0309 <manav2005dhameliya@gmail.com>
2024-08-08 16:42:21 -0400

    adding my part of converting module version_3

---
+++ Source Code/Version 3/Converting.h
@@ -0,0 +1,16 @@
+#pragma once
+// CONVERTING MODULE HEADER
+
+#ifndef CONVERTING_H
+
+#define CONVERTING_H
+
+#include <stdio.h>
+
+#include <string.h>
+
+#include <stdlib.h>
+
+void converting(void);
+
+#endif
